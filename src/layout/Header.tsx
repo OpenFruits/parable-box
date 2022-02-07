32ef56c6c075c36435a7cf826d4c4429483360f4
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-08 05:43:12 +0900

    fix: header and navbar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,15 +2,15 @@ import type { VFC } from "react";
 import { NavLinks } from "src/component/NavLinks";
 
 const NAV_ITEMS = [
-  { href: "/", label: "Root" },
-  { href: "/about", label: "About" },
-  { href: "/components", label: "Components" },
+  { href: "/", label: "投稿一覧" },
+  { href: "/liked", label: "お気に入り" },
+  { href: "/create", label: "新規作成" },
 ];
 
 export const Header: VFC = () => {
   return (
     <header>
-      <h1 className="text-4xl">App Title</h1>
+      <h1 className="text-4xl">Parable Box</h1>
       <nav className="text-gray-500">
         {NAV_ITEMS.map((item) => {
           const { href, label } = item;
