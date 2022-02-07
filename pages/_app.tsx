7f13bc86b654f35244099001218c5da27720e6f3
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-08 06:54:47 +0900

    add: ClerkProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../src/style/index.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import type { CustomAppProps } from "next/app";
 import Head from "next/head";
 
@@ -16,7 +17,7 @@ const MyApp = (props: CustomAppProps) => {
         <meta name="description" content="Starter Template by Next.js and TailwindCSS." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {getLayout(<props.Component {...props.pageProps} />)}
+      <ClerkProvider>{getLayout(<props.Component {...props.pageProps} />)}</ClerkProvider>
     </>
   );
 };
