5bcd81118813a93fb1c215bac51947eee57bbc0e
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-09 20:01:15 +0900

    set Supabase JWT on the client object

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,3 +4,11 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   process.env.NEXT_PUBLIC_SUPABASE_KEY || ""
 );
+
+export const supabaseClient = async (supabaseAccessToken: string) => {
+  // set Supabase JWT on the client object,
+  // so it is sent up with all Supabase requests
+  supabase.auth.setAuth(supabaseAccessToken);
+
+  return supabase;
+};
