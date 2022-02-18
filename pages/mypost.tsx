22fb257e1adf3bee2a610e96f2557b0979e4d531
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-18 15:19:05 +0900

    refactor

--- pages/mypost.tsx
+++ pages/mypost.tsx
@@ -1,57 +1,11 @@
-import { useSession, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import type { CustomNextPage } from "next";
 import Head from "next/head";
-import type { VFC } from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { MyAbstractList } from "src/component/MyAbstractList";
 import { SessionLoading } from "src/component/SessionLoading";
 import { FixedLayout } from "src/layout/FixedLayout";
 import type { Abstract } from "src/type/data";
-import { supabaseClient } from "src/utils/supabase";
-
-type Props = {
-  abstracts: Abstract[];
-  setAbstracts: React.Dispatch<React.SetStateAction<Abstract[]>>;
-};
-
-const MyAbstractList: VFC<Props> = (props) => {
-  const { session } = useSession();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const loadMyAbstracts = async () => {
-      try {
-        setIsLoading(true);
-        const supabaseAccessToken = await session?.getToken({
-          template: "Supabase",
-        });
-        const supabase = await supabaseClient(supabaseAccessToken as string);
-        const { data: abstracts } = await supabase
-          .from<Abstract>("abstracts")
-          .select("*")
-          .match({ user_id: session.user.id });
-        abstracts && props.setAbstracts(abstracts);
-      } catch (e) {
-        alert(e);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    loadMyAbstracts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return props.abstracts?.length > 0 ? (
-    <ul>
-      {props.abstracts?.map((abstract: Abstract) => (
-        <li key={abstract.id}>{abstract.body}</li>
-      ))}
-    </ul>
-  ) : (
-    <div>No Abstracts!</div>
-  );
-};
 
 const MyPost: CustomNextPage<{ abstracts: Abstract[] }> = () => {
   const { isSignedIn, isLoaded, user } = useUser();
