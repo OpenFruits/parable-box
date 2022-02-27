5ccba4b4cca7564ca66288cb57cb83b090c82308
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-27 23:57:05 +0900

    update: card ui

--- src/component/AbstractCard.tsx
+++ src/component/AbstractCard.tsx
@@ -3,9 +3,13 @@ import type { Abstract } from "src/type/data";
 
 export const AbstractCard: VFC<{ abstract: Abstract }> = (props) => {
   return (
-    <div className="group relative my-6">
-      <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg opacity-75 group-hover:opacity-100 blur transition duration-1000 group-hover:duration-200"></div>
-      <div className="relative py-3 px-6 text-black bg-gray-100 rounded-lg">{props.abstract.body}</div>
+    <div>
+      <div className="group relative my-6">
+        <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 hover:from-pink-900 to-purple-600 hover:to-purple-900 rounded-lg opacity-75 group-hover:opacity-100 blur transition duration-200 group-hover:duration-200"></div>
+        <div className="relative py-3 px-6 text-gray-600 hover:text-gray-900 bg-gray-100 hover:bg-white rounded-lg">
+          <div>{props.abstract.body}</div>
+        </div>
+      </div>
     </div>
   );
 };
