c3ae2f2d5a76e9488ec985b65d513f6d2abedd33
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-01 05:11:31 +0900

    add: new component

---
+++ src/component/ParableCard.tsx
@@ -0,0 +1,15 @@
+import type { VFC } from "react";
+import type { Parable } from "src/type/data";
+
+export const ParableCard: VFC<{ parable: Parable }> = (props) => {
+  return (
+    <div>
+      <div className="group relative my-6">
+        <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 hover:from-pink-900 to-purple-600 hover:to-purple-900 rounded-lg opacity-75 group-hover:opacity-100 blur transition duration-200 group-hover:duration-200"></div>
+        <div className="relative py-3 px-6 text-gray-600 hover:text-gray-900 bg-gray-100 hover:bg-white rounded-lg">
+          <div>{props.parable.body}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
