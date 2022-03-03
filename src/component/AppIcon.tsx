e4327c6ba72d0f1fdb31bcfed69b98c8bf614ba5
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-03 23:52:43 +0900

    rename: AppLogo -> AppIcon

--- src/component/AppIcon.tsx
+++ src/component/AppIcon.tsx
@@ -1,7 +1,7 @@
 import { ChartPieIcon } from "@heroicons/react/solid";
 import type { VFC } from "react";
 
-export const AppLogo: VFC = () => {
+export const AppIcon: VFC = () => {
   return (
     <ChartPieIcon className="w-10 h-10 text-black bg-gradient-to-r from-pink-600 hover:from-pink-900 to-purple-600 rounded-md" />
   );
