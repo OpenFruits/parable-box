c9146bd273419d82913331d4f3448bf8dca6acb3
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-04 16:23:23 +0900

    update: logo color

--- src/component/AppLogo.tsx
+++ src/component/AppLogo.tsx
@@ -3,7 +3,7 @@ import { AppIcon } from "src/component/AppIcon";
 
 export const AppLogo: VFC = () => {
   return (
-    <div className="flex space-x-2 font-mono text-4xl font-semibold text-gray-900">
+    <div className="flex space-x-2 font-mono text-4xl font-semibold text-slate-800">
       <AppIcon />
       <p>Parable</p>
       <p>Box</p>
