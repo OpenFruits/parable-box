6ee3b0ea0bf07f662bd28aabe50be13069b3a044
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-07 21:36:50 +0900

    feat: add hooks to fetch myposts

--- src/component/MyAbstractList.tsx
+++ src/component/MyAbstractList.tsx
@@ -1,51 +1,23 @@
-import { useSession } from "@clerk/nextjs";
 import Link from "next/link";
 import type { VFC } from "react";
-import { useEffect, useState } from "react";
 import { AbstractCard } from "src/component/AbstractCard";
 import { FetchLoading } from "src/component/Loading/FetchLoading";
 import { NoAbstracts } from "src/component/NoAbstracts";
+import { useMyPostList } from "src/hooks/useMyPostList";
 import type { Abstract } from "src/type/data";
-import { supabaseClient } from "src/utils/supabase";
 
 export const MyAbstractList: VFC = () => {
-  const { session } = useSession();
-  const [isLoading, setIsLoading] = useState(true);
-  const [abstracts, setAbstracts] = useState<Abstract[]>([]);
-
-  useEffect(() => {
-    const loadMyAbstracts = async () => {
-      try {
-        setIsLoading(true);
-        const supabaseAccessToken = await session?.getToken({
-          template: "Supabase",
-        });
-        const supabase = await supabaseClient(supabaseAccessToken as string);
-        const { data } = await supabase
-          .from<Abstract>("abstracts")
-          .select("*")
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          .match({ user_id: session.user.id });
-        data && setAbstracts(data);
-      } catch (e) {
-        alert(e);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    loadMyAbstracts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { abstracts, isLoading } = useMyPostList();
 
   if (isLoading) return <FetchLoading />;
 
   return abstracts?.length > 0 ? (
-    <ul>
+    <ul className="py-4">
       {abstracts?.map((abstract: Abstract) => (
         <li key={abstract.id}>
           <Link href={`/abstract/${abstract.id}`}>
             <a>
-              <AbstractCard abstract={abstract} />
+              <AbstractCard abstract={abstract} hasLink />
             </a>
           </Link>
         </li>
---
+++ src/hooks/useMyPostList.ts
@@ -0,0 +1,36 @@
+import { useSession } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import type { Abstract } from "src/type/data";
+import { supabaseClient } from "src/utils/supabase";
+
+export const useMyPostList = () => {
+  const { session } = useSession();
+  const [abstracts, setAbstracts] = useState<Abstract[]>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadMyAbstracts = async () => {
+      try {
+        setIsLoading(true);
+        const supabaseAccessToken = await session?.getToken({
+          template: "Supabase",
+        });
+        const supabase = await supabaseClient(supabaseAccessToken as string);
+        const { data } = await supabase
+          .from<Abstract>("abstracts")
+          .select("*")
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          .match({ user_id: session.user.id });
+        data && setAbstracts(data);
+      } catch (e) {
+        alert(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadMyAbstracts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { abstracts, isLoading };
+};
